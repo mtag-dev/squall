fe7f0daf6e109db82674e45c1ce6cbf25dbc663c
Stanislav Dubrovskyi <s.dubrovskyi@cleverdec.com>
2021-10-28 13:49:22 +0300

    [SQUALL-7] Fix typing test

--- tests/test_typing_python39.py
+++ tests/test_typing_python39.py
@@ -1,4 +1,3 @@
-import pytest
 from squall import Squall
 from squall.testclient import TestClient
 
@@ -6,22 +5,21 @@ from .utils import needs_py39
 
 
 @needs_py39
-@pytest.mark.parametrize(
-    "test_type,expect",
-    [
+def test_typing():
+    checks = [
         (list[int], [1, 2, 3]),
         (dict[str, list[int]], {"a": [1, 2, 3], "b": [4, 5, 6]}),
         (tuple[int, ...], [1, 2, 3]),
         (set[int], [1, 2, 3]),  # `set` is converted to `tuple`
-    ],
-)
-def test_typing(test_type, expect):
-    app = Squall()
+    ]
 
-    @app.post("/", response_model=test_type)
-    def post_endpoint(input: test_type):
-        return input
+    for test_type, expect in checks:
+        app = Squall()
 
-    res = TestClient(app).post("/", json=expect)
-    assert res.status_code == 200, res.json()
-    assert res.json() == expect
+        @app.post("/", response_model=test_type)
+        def post_endpoint(input: test_type):
+            return input
+
+        res = TestClient(app).post("/", json=expect)
+        assert res.status_code == 200, res.json()
+        assert res.json() == expect
